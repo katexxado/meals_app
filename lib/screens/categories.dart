be4823e74ec49868ff2e55cc84c9202a08223f38
katexxado <adasikatumu1@gmail.com>
2023-11-24 16:50:05 +0100

    added

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,9 +3,12 @@ import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import '../models/category.dart';
+import 'package:meals_app/models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
@@ -15,6 +18,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
@@ -22,11 +26,7 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
-      body: GridView(
+    return GridView(
         padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -40,7 +40,6 @@ class CategoriesScreen extends StatelessWidget {
               _selectCategory(context, category);
             },)
         ],
-      ),
-    );
+      );
   }
 }
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,36 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/screens/meal_details.dart';
 import 'package:transparent_image/transparent_image.dart';
 import 'package:meals_app/models/meal.dart';
-
+import 'package:meals_app/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({
+    super.key,
+    required this.meal,
+    required this.onSelectmeal,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectmeal;
+
+  String get complexityText {
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
+  String get affordableText {
+    return meal.affordability.name[0].toUpperCase() + meal.affordability.name.substring(1);
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.all(8),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
-        onTap: (){},
+        onTap: () {
+          onSelectmeal(meal);
+        },
         child: Stack(
           children: [
-            FadeInImage(placeholder: MemoryImage(kTransparentImage),
-            image: NetworkImage(meal.imageUrl),
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
             Positioned(
               bottom: 0,
               left: 0,
               right: 0,
               child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 44, vertical: 6),
+                padding: const EdgeInsets.symmetric(horizontal: 44, vertical: 6),
                 color: Colors.black54,
                 child: Column(
                   children: [
-                    Text(meal.title, maxLines: 2, textAlign: TextAlign.center, ),
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12,),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: complexityText,
+                        ),
+                        const SizedBox(width: 12,),
+                        MealItemTrait(
+                          icon: Icons.attach_money,
+                          label: affordableText,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-            ),
+              ),
             ),
           ],
         ),
